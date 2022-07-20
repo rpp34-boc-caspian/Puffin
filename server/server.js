8e5e92e8377d5606a648601bc1a53416a51ebdca
Keegan Wolf <kwolf313@live.com>
2022-07-20 16:52:53 -0500

    Added basic express logic to the server file

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
