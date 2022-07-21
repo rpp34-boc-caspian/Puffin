59c1dbaae04d0c86799255b4d80a2beac0e47972
Jane Srisarakorn <janedoenut@gmail.com>
2022-07-20 18:19:51 -0700

    Attempt fix and remove dups

--- src/Daily/Daily.tsx
+++ src/Daily/Daily.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-// import './Daily.css';
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import format from 'date-fns/format';
 import parse from 'date-fns/parse';
@@ -9,10 +7,6 @@ import getDay from 'date-fns/getDay';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 
-// eslint-disable-next-line
-import BigCalendar from "react-big-calendar";
-// eslint-disable-next-line
-import moment from "moment";
 
 const locales = {
   'en-US': require('date-fns/locale/en-US')
@@ -50,9 +44,9 @@ function Daily() {
   const [newEvent, setNewEvent] = useState({title: "", start: "", end: ""})
   const [allEvents, setAllEvents] = useState(events)
 
-  // function handleAddEvent() {
-  //   setAllEvents([...allEvents, newEvent])
-  // }
+    // function handleAddEvent() {
+    //   setAllEvents([...allEvents, newEvent])
+    // }
 
   return (
     <div className="Daily">
