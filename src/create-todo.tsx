03f06a7f29d5ff203b039088d2aee2f30a8cdc36
Tam <89226497+tpminhtam@users.noreply.github.com>
2022-07-20 23:55:51 -0700

    setting up all inputs for create-todo form

--- src/create-todo.tsx
+++ src/create-todo.tsx
@@ -9,8 +9,12 @@ import Stack from '@mui/material/Stack';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import { FilledInput, FormControl, InputAdornment, InputLabel, Typography } from '@mui/material';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { FilledInput, FormControl, InputAdornment, InputLabel, Typography, Button, OutlinedInput, MenuItem, ListItemText, Checkbox } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import PeopleIcon from '@mui/icons-material/People';
+import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -27,6 +31,27 @@ const Container = styled('div')`
     // align-items: center;
 `;
 
+const StyledPeopleIcon = styled(PeopleIcon)`
+    margin: 0 10px;
+`
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const names = [
+    'Tam',
+    'someone',
+    'test',
+];
+
 export function Todo() {
 
     const label = { inputProps: { 'aria-label': 'Switch demo' } };
@@ -39,10 +64,22 @@ export function Todo() {
         setValue(newValue);
     };
 
+    const [personName, setPersonName] = React.useState<string[]>([]);
+
+    const handleChangeCategories = (event: SelectChangeEvent<typeof personName>) => {
+        const {
+            target: { value },
+        } = event;
+        setPersonName(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
     return (
         <Container>
             <Stack spacing={5}>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div style={{ display: 'flex', justifyContent: 'left' }}>
                     <Typography variant="h4" component="h2" >
                         Add a new to-do
                     </Typography>
@@ -50,7 +87,7 @@ export function Todo() {
                 <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                     <TextField
                         id="standard-textarea"
-                        label="Title"
+                        label="TITLE"
                         placeholder="Enter Title"
                         multiline
                         variant="standard"
@@ -61,11 +98,11 @@ export function Todo() {
                 <Grid container item xs={12}>
                     <TextField
                         id="outlined-multiline-static"
-                        // label="Description"
+                        label="DESCRIPTION"
                         fullWidth
                         multiline
                         rows={6}
-                        defaultValue="Enter Description"
+                        placeholder="Enter Description"
                     />
                 </Grid>
                 <div>
@@ -73,12 +110,12 @@ export function Todo() {
                 </div>
                 <LocalizationProvider dateAdapter={AdapterDateFns}>
                     <Grid container spacing={3}>
-                        <Grid container item xs={12}>
-                            <Grid item xs={4}>
+                        <Grid container item xs={12} spacing={2}>
+                            <Grid item xs={2}>
                                 Start
                             </Grid>
-                            <Grid item xs={4}>
-                                <MobileDatePicker
+                            <Grid item xs={5}>
+                                <DesktopDatePicker
                                     label="Date"
                                     inputFormat="MM/dd/yyyy"
                                     value={value}
@@ -86,21 +123,27 @@ export function Todo() {
                                     renderInput={(params) => <TextField {...params} />}
                                 />
                             </Grid>
-                            <Grid item xs={4}>
-                                <TimePicker
+                            <Grid item xs={5}>
+                                <MobileTimePicker
                                     label="Time"
                                     value={value}
                                     onChange={handleChange}
                                     renderInput={(params) => <TextField {...params} />}
                                 />
+                                {/* <TimePicker
+                                    label="Time"
+                                    value={value}
+                                    onChange={handleChange}
+                                    renderInput={(params) => <TextField {...params} />}
+                                /> */}
                             </Grid>
                         </Grid>
-                        <Grid container item xs={12}>
-                            <Grid item xs={4}>
+                        <Grid container item xs={12} spacing={2}>
+                            <Grid item xs={2}>
                                 End
                             </Grid>
-                            <Grid item xs={4}>
-                                <MobileDatePicker
+                            <Grid item xs={5}>
+                                <DesktopDatePicker
                                     label="Date"
                                     inputFormat="MM/dd/yyyy"
                                     value={value}
@@ -108,7 +151,7 @@ export function Todo() {
                                     renderInput={(params) => <TextField {...params} />}
                                 />
                             </Grid>
-                            <Grid item xs={4}>
+                            <Grid item xs={5}>
                                 <TimePicker
                                     label="Time"
                                     value={value}
@@ -120,10 +163,44 @@ export function Todo() {
                     </Grid>
                 </LocalizationProvider>
                 <div>
-                    Share with friends <Switch {...label} defaultChecked />
+                    <Checkbox {...label} /> All day
                 </div>
+                <div>
+                    <StyledPeopleIcon />Share with friends<Switch {...label} defaultChecked />
+                </div>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                    <InputLabel id="demo-multiple-checkbox-label">CATEGORIES</InputLabel>
+                    <Select
+                        labelId="demo-multiple-checkbox-label"
+                        id="demo-multiple-checkbox"
+                        multiple
+                        value={personName}
+                        onChange={handleChangeCategories}
+                        input={<OutlinedInput label="Tag" />}
+                        renderValue={(selected) => selected.join(', ')}
+                        MenuProps={MenuProps}
+                    >
+                        {names.map((name) => (
+                            <MenuItem key={name} value={name}>
+                                <Checkbox checked={personName.indexOf(name) > -1} />
+                                <ListItemText primary={name} />
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <Grid container>
+                    <Grid xs={9}></Grid>
+                    <Grid xs={3}>
+                        <Button fullWidth variant="contained" size="large">Add</Button>\
+                    </Grid>
+
+                </Grid>
+                {/* <div style={{ display: 'flex', justifyContent: 'right' }}> */}
+                {/* </div> */}
             </Stack>
         </Container>
     )
 
 }
+
+
