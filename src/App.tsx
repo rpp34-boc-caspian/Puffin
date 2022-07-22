1ed22f8a8b08f7071e22cd450a962f11ec7bafe3
Darian Hogue <hogued96@gmail.com>
2022-07-22 15:41:09 -0400

    fixed npm issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Metrics } from './Metrics/Metrics';
 
-const todoTemplate = {
-  title: 'Clean Up',
-  start_date: 'Date',
-  end_date: 'Date',
-  complete: true,
-  username: 'JimmyJoeBob'
-}
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <h2>Watch Tower Up and Running!!</h2>
-      <Metrics todos={todoTemplate}></Metrics>
     </div>
   );
 }
