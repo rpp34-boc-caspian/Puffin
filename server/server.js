9dda10aea9f383264962af7b6d2e134d8bf5684b
Isaiah Smith <isaiah.cornelius.smith@gmail.com>
2022-07-23 12:06:24 -0700

    Changes to database

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8080;
+const pool = require('../db/index.js');
 
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.post('/signup', (req, res) => {
@@ -28,7 +29,7 @@ app.post('/login', (req, res) => {
   console.log('REQUEST', req.body);
 
   // Request to database for username provided
-    // Check for what a failed request returns
+  // Check for what a failed request returns
 
   // If username doesn't exist in database, return false
 
@@ -36,7 +37,48 @@ app.post('/login', (req, res) => {
 
   // Return compare's result
 
-  res.json({exist: true});
+  res.json({ exist: true });
+
+  //Sharing Functions
+  /*
+  app.get('calendar', (req, res) => {
+    const user_id = req.params.user_id;
+    pool.query(`SELECT cal_name FROM calendars LEFT JOIN users WHERE user_id = ${user_id}`)
+      .then(output => {
+        res.send(output);
+      });
+  })
+
+  app.get('/friends', (req, res) => {
+    const user_id = req.params.user_id;
+    pool.query(`SELECT friend_id FROM friends WHERE user_id = ${user_id}`)
+      .then(output => {
+        res.send(output);
+      });
+  });
+
+  app.post('/friends', (req, res) => {
+    const query = `INSERT INTO friends(user_id, friend_id) VALUES (${req.params.user_id}, ${req.params.friend_id})`;
+    pool.query(query, (err, data) => {
+    if (err) {
+      console.log('err1: ', err);
+      res.status(500).send(err);
+    } else {
+      query.push([friend_id]);
+    }});
+
+  app.post('/share', (req, res) => {
+      const query = `INSERT INTO user(user_id, friend_id) VALUES (${req.params.user_id}, ${req.params.friend_id})`;
+      pool.query(query, (err, data) => {
+      if (err) {
+        console.log('err1: ', err);
+        res.status(500).send(err);
+      } else {
+          query.characteristicReviewsValues.push([friend_id]);
+      };
+  });*/
+
+  //END of Sharing Functions
 });
 
 app.listen(port, () => {
---
+++ src/sharing/share.tsx
@@ -0,0 +1,31 @@
+import { Container, Stack } from "@mui/material"
+import { useState } from "react"
+
+function Share() {
+  return (
+    <>
+      <div>
+        <label>
+          Sharing With Friends
+        </label>
+      </div>
+      <div>
+        <label>
+          Select Calendar / Categories / Events
+        </label>
+      </div>
+      <div>
+        <label>
+          Share With Friends
+        </label>
+      </div>
+      <div>
+        <label>
+          Add Friends
+        </label>
+      </div>
+    </>
+  );
+};
+
+export default Share;
