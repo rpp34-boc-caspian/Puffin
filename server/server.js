539a24c7903507a54e3a55978c8eace0e5d458c6
Isaiah Smith <isaiah.cornelius.smith@gmail.com>
2022-07-28 08:44:28 -0700

    Fixing server issue

--- server/server.js
+++ server/server.js
@@ -15,31 +15,31 @@ app.post('/signup', async (req, res) => {
   let hash = await bcrypt.hash(password, 11);
 
   pool.query(`SELECT * FROM users WHERE email = '${email}' OR username = '${username}'`)
-  .then((results) => {
-    let query = results.rows[0];
+    .then((results) => {
+      let query = results.rows[0];
 
-    if (results.rows.length > 0) {
-      query.email === email ? res.json({ created: false, reason: 'email' }) : res.json({ created: false, reason: 'username' });
+      if (results.rows.length > 0) {
+        query.email === email ? res.json({ created: false, reason: 'email' }) : res.json({ created: false, reason: 'username' });
 
-    } else {
+      } else {
 
-      pool.query(`INSERT INTO users (email, username, hashed_pass) VALUES ('${email}', '${username}', '${hash}')`)
-      .then((result) => {
-        res.json({ created: true });
+        pool.query(`INSERT INTO users (email, username, hashed_pass) VALUES ('${email}', '${username}', '${hash}')`)
+          .then((result) => {
+            res.json({ created: true });
 
-      })
-      .catch(() => {
-        res.status(500).send();
+          })
+          .catch(() => {
+            res.status(500).send();
 
-      });
+          });
 
-    }
+      }
 
-  })
-  .catch(() => {
-    res.status(500).send();
+    })
+    .catch(() => {
+      res.status(500).send();
 
-  });
+    });
 
 });
 
@@ -47,52 +47,69 @@ app.post('/login', (req, res) => {
   let { username, email, password } = req.body;
 
   pool.query(`SELECT * FROM users WHERE username = '${req.body.username}'`)
-  .then((results) => {
-    if (results.rows.length === 0) {
-      res.json({ exists: false });
+    .then((results) => {
+      if (results.rows.length === 0) {
+        res.json({ exists: false });
 
-    };
+      };
 
-    if (username !== results.rows[0].username) {
-      res.json({ exists: false });
+      if (username !== results.rows[0].username) {
+        res.json({ exists: false });
 
-    };
+      };
 
-    if (email !== results.rows[0].email) {
-      res.json({ exists: false });
+      if (email !== results.rows[0].email) {
+        res.json({ exists: false });
 
-    };
+      };
 
-    let match = bcrypt.compare(password, results.rows[0].password);
+      let match = bcrypt.compare(password, results.rows[0].password);
 
-    if (!match) {
-      res.json({ exists: false });
+      if (!match) {
+        res.json({ exists: false });
 
-    };
+      };
 
-    res.json({ exists: true });
+      res.json({ exists: true });
 
-  //Sharing Functions
+    })
+    .catch(() => {
+      res.status(500).send();
 
-  app.get('calendar_todos', (req, res) => {
-    const user_id = req.params.user_id;
-    pool.query(`SELECT cal_name FROM calendars WHERE user_id = ${user_id}`)
-      .then(output => {
-        res.send(output);
-      });
-  })
+    });
 
-  app.get('/share_todos_info', (req, res) => {
-    const user_id = req.params.user_id;
-    pool.query(`SELECT calendars.user_id, calendars.cal_name, categories.category_name, categories.color, todos.title, users.username AS friend , permissions.permission FROM calendars LEFT JOIN categories ON calendars.user_id = categories.calendar_id LEFT JOIN todos ON categories.id = todos.cat_id LEFT JOIN permissions ON permissions.user_id = calendars.user_id LEFT JOIN users ON permissions.friend_id = users.id WHERE calendars.user_id = ${user_id}`)
-      .then(output => {
-        res.send(output);
-      });
-  });
+});
+//Sharing Functions
 
-  /*
-  app.post('/friends', (req, res) => {
-  const query = `INSERT INTO friends(user_id, friend_id) VALUES (${req.params.user_id}, ${req.params.friend_id})`;
+app.get('calendar_todos', (req, res) => {
+  const user_id = req.params.user_id;
+  pool.query(`SELECT cal_name FROM calendars WHERE user_id = ${user_id}`)
+    .then(output => {
+      res.send(output);
+    });
+});
+
+app.get('/share_todos_info', (req, res) => {
+  const user_id = req.params.user_id;
+  pool.query(`SELECT calendars.user_id, calendars.cal_name, categories.category_name, categories.color, todos.title, users.username AS friend , permissions.permission FROM calendars LEFT JOIN categories ON calendars.user_id = categories.calendar_id LEFT JOIN todos ON categories.id = todos.cat_id LEFT JOIN permissions ON permissions.user_id = calendars.user_id LEFT JOIN users ON permissions.friend_id = users.id WHERE calendars.user_id = ${user_id}`)
+    .then(output => {
+      res.send(output);
+    });
+});
+
+/*
+app.post('/friends', (req, res) => {
+const query = `INSERT INTO friends(user_id, friend_id) VALUES (${req.params.user_id}, ${req.params.friend_id})`;
+pool.query(query, (err, data) => {
+if (err) {
+  console.log('err1: ', err);
+  res.status(500).send(err);
+} else {
+  query.push([friend_id]);
+}});
+
+app.post('/share', (req, res) => {
+  const query = `INSERT INTO user(user_id, friend_id) VALUES (${req.params.user_id}, ${req.params.friend_id})`;
   pool.query(query, (err, data) => {
   if (err) {
     console.log('err1: ', err);
@@ -101,27 +118,17 @@ app.post('/login', (req, res) => {
     query.push([friend_id]);
   }});
 
-  app.post('/share', (req, res) => {
+
+app.post('/share', (req, res) => {
     const query = `INSERT INTO user(user_id, friend_id) VALUES (${req.params.user_id}, ${req.params.friend_id})`;
     pool.query(query, (err, data) => {
     if (err) {
       console.log('err1: ', err);
       res.status(500).send(err);
     } else {
-      query.push([friend_id]);
-    }});
-
-
-  app.post('/share', (req, res) => {
-      const query = `INSERT INTO user(user_id, friend_id) VALUES (${req.params.user_id}, ${req.params.friend_id})`;
-      pool.query(query, (err, data) => {
-      if (err) {
-        console.log('err1: ', err);
-        res.status(500).send(err);
-      } else {
-          query.characteristicReviewsValues.push([friend_id]);
-      };
-  });*/
+        query.characteristicReviewsValues.push([friend_id]);
+    };
+});*/
 
 //END of Sharing Functions
 
@@ -130,12 +137,12 @@ app.get('/unscheduledTodos/:userId', (req, res) => {
   const user_id = req.params.userId;
   const query = 'SELECT t.id, t.title, t.descript, t.complete, c.color FROM todos t LEFT JOIN categories c ON t.cat_id = c.id WHERE t.user_id = $1 AND t.complete = false;'
   pool.query(query, [user_id])
-  .then(({rows}) => {
-    res.send(rows);
-  })
-  .catch(err => {
-    res.status(500).send(err);
-  })
+    .then(({ rows }) => {
+      res.send(rows);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    })
 })
 
 app.put('/unscheduledTodos/:todoId', (req, res) => {
@@ -144,43 +151,43 @@ app.put('/unscheduledTodos/:todoId', (req, res) => {
   const updateTrueQuery = 'UPDATE todos SET complete = true WHERE id = $1';
   const updateFalseQuery = 'UPDATE todos SET complete = false WHERE id = $1';
   pool.query(searchQuery, [todo_id])
-  .then(({rows}) => {
-    return rows[0].complete;
-  })
-  .then(complete => {
-    console.log('complete', complete);
-    if (complete) {
-      return pool.query(updateFalseQuery, [todo_id])
-    } else {
-      return pool.query(updateTrueQuery, [todo_id])
-    }
-  })
-  .then(({rowCount}) => {
-    if (rowCount > 0) {
-      res.status(204).json({status:"updated"});
-    }
-  })
-  .catch(err => {
-    console.log('err', err);
-    res.status(500).send(err);
-  })
+    .then(({ rows }) => {
+      return rows[0].complete;
+    })
+    .then(complete => {
+      console.log('complete', complete);
+      if (complete) {
+        return pool.query(updateFalseQuery, [todo_id])
+      } else {
+        return pool.query(updateTrueQuery, [todo_id])
+      }
+    })
+    .then(({ rowCount }) => {
+      if (rowCount > 0) {
+        res.status(204).json({ status: "updated" });
+      }
+    })
+    .catch(err => {
+      console.log('err', err);
+      res.status(500).send(err);
+    })
 })
 
 app.delete('/unscheduledTodos/:todoId', (req, res) => {
   const todo_id = req.params.todoId;
   const query = 'DELETE FROM todos WHERE id = $1;';
   pool.query(query, [todo_id])
-  .then(({rowCount}) => {
-    if (rowCount > 0) {
-      res.status(204).json({status:"deleted"});
-    }
-  })
-  .catch(err => {
-    console.log('err', err);
-    res.status(500).send(err);
-  })
+    .then(({ rowCount }) => {
+      if (rowCount > 0) {
+        res.status(204).json({ status: "deleted" });
+      }
+    })
+    .catch(err => {
+      console.log('err', err);
+      res.status(500).send(err);
+    })
 })
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
-});
+})
